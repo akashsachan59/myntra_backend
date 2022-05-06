3a4ca961de24ce30e5b84c96785eab45ef7a09db
Akash Sachan <akashsachan@Akashs-MacBook-Air.local>
2022-05-06 23:15:36 +0530

    modified delete cart/wishlist

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
- 
+
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -12,7 +12,7 @@ app.use(cors({
 const port = 3000
 
 const Airtable = require('airtable');
-const base = new Airtable({apiKey: 'keysVKJT0WBapnQ7a'}).base('applbqSXlfIiMjO7A')
+const base = new Airtable({ apiKey: 'keysVKJT0WBapnQ7a' }).base('applbqSXlfIiMjO7A')
 
 // login
 app.post('/login', (req, res) => {
@@ -20,7 +20,7 @@ app.post('/login', (req, res) => {
     console.log()
     let reqEmail = req.body.email
     let reqPassword = req.body.password
-    console.log(reqEmail,reqPassword)
+    console.log(reqEmail, reqPassword)
     base('user').select({
         filterByFormula: `{email} = "${reqEmail}"`
     }).firstPage(function (err, records) {
@@ -44,7 +44,7 @@ app.post('/login', (req, res) => {
                 }
                 res.status(200)
                 res.send(obj)
-            }else{
+            } else {
                 let obj = {
                     token: 'wrong password',
                     isloggedin: false
@@ -52,7 +52,7 @@ app.post('/login', (req, res) => {
                 res.status(404)
                 res.send(obj)
             }
-        }else{
+        } else {
             let obj = {
                 token: 'No user found',
                 isloggedin: false
@@ -71,25 +71,25 @@ app.post('/signup', (req, res) => {
     let reqEmail = req.body.email
     let reqPassword = req.body.password
     let reqAddresses = req.body.addresses
-    
+
     base('user').create([
         {
-          "fields": {
-            "name": `${reqName}`,
-            "gender": `${reqGender}`,
-            "phone": `${reqPhone}`,
-            "email": `${reqEmail}`,
-            "password": `${reqPassword}`,
-            "addresses": `${reqAddresses}`
-          }
+            "fields": {
+                "name": `${reqName}`,
+                "gender": `${reqGender}`,
+                "phone": `${reqPhone}`,
+                "email": `${reqEmail}`,
+                "password": `${reqPassword}`,
+                "addresses": `${reqAddresses}`
+            }
         }
-      ], function(err, records) {
+    ], function (err, records) {
         if (err) {
-          res.status(500)
-          res.send('error')  
-          console.error(err);
-          return;
-        }else{
+            res.status(500)
+            res.send('error')
+            console.error(err);
+            return;
+        } else {
             let obj = {
                 user: true,
                 msg: "user successfully created"
@@ -97,18 +97,18 @@ app.post('/signup', (req, res) => {
             res.status(200)
             res.send(obj)
         }
-      });
+    });
 
 })
 
 // List all products
-app.get('/home', (req,res) => {
+app.get('/home', (req, res) => {
     base('product').select({
         view: 'Grid view'
-    }).firstPage(function(err, records) {
+    }).firstPage(function (err, records) {
         if (err) { console.error(err); return; }
         let data = []
-        records.forEach(function(record) {
+        records.forEach(function (record) {
             console.log('Retrieved', record.get('name'));
             data.push(record.fields)
         });
@@ -117,14 +117,14 @@ app.get('/home', (req,res) => {
 })
 
 // show detail product
-app.get('/product', (req,res) => {
+app.get('/product', (req, res) => {
     base('product').select({
         filterByFormula: `id = "${req.query.id}"`
-    }).firstPage(function(err, records) {
+    }).firstPage(function (err, records) {
         if (err) {
             res.status(404).send('product not found')
             console.error(err)
-            return 
+            return
         }
         let obj = records[0].fields
         res.status(200).send(obj)
@@ -135,22 +135,22 @@ app.get('/product', (req,res) => {
 app.get('/search', (req, res) => {
     base('product').select({
         view: 'Grid view'
-    }).firstPage(function(err, records) {
-        if (err) { 
+    }).firstPage(function (err, records) {
+        if (err) {
             console.error(err)
-            return 
+            return
         }
         let data = []
         let searchKey = req.query.search.toLowerCase()
-        records.forEach(function(record) {
-            data.push(record.fields)    
+        records.forEach(function (record) {
+            data.push(record.fields)
         })
         const foundData = data.filter((e) => {
-            return(e.name.toLowerCase().includes(searchKey))
+            return (e.name.toLowerCase().includes(searchKey))
         })
-        
+
         res.status(200).send(foundData)
-        
+
     });
 })
 
@@ -158,32 +158,32 @@ app.get('/search', (req, res) => {
 app.get('/filter', (req, res) => {
     base('product').select({
         view: 'Grid view'
-    }).firstPage(function(err, records) {
-        if (err) { 
+    }).firstPage(function (err, records) {
+        if (err) {
             console.error(err)
-            return 
+            return
         }
         let data = []
         let searchKey = req.query.search.toLowerCase()
-        records.forEach(function(record) {
-            data.push(record.fields)    
+        records.forEach(function (record) {
+            data.push(record.fields)
         })
         const foundData = data.filter((e) => {
-            return(e.name.toLowerCase().includes(searchKey))
+            return (e.name.toLowerCase().includes(searchKey))
         })
-        
+
         res.status(200).send(foundData)
-        
+
     });
 })
 
 // sort by price
 app.get('/sort', (req, res) => {
     base('product').select({
-        sort: [{field: "price", direction: `${req.query.direction}`}]
-    }).firstPage(function(err, records) {
+        sort: [{ field: "price", direction: `${req.query.direction}` }]
+    }).firstPage(function (err, records) {
         if (err) { console.error(err); return; }
-        records.forEach(function(record) {
+        records.forEach(function (record) {
             console.log('Retrieved', record.fields);
         });
     });
@@ -193,14 +193,14 @@ app.get('/sort', (req, res) => {
 app.post('/addToCart', (req, res) => {
     base('user').find(`${req.body.user_id}`, function (err, record) {
         const reqId = req.body.id
-        if (err) { 
+        if (err) {
             res.status(404)
             res.send('user not found')
             console.error(err)
             return
         }
         if (!record.get('cart')) {
-            let obj = [{id: reqId}]
+            let obj = [{ id: reqId }]
             let id = JSON.stringify(obj)
             console.log(id)
             base('user').update([
@@ -214,22 +214,22 @@ app.post('/addToCart', (req, res) => {
                 if (err) {
                     console.error(err);
                     return;
-                }else{
+                } else {
                     res.send('Added to cart')
                 }
 
             });
         } else {
             let id = JSON.parse(record.get('cart'))
-            let checkDuplicate = id.find(rec => rec.id === `${reqId}`) 
-            if(checkDuplicate){
+            let checkDuplicate = id.find(rec => rec.id === `${reqId}`)
+            if (checkDuplicate) {
                 let obj = {
                     msg: 'product already in cart'
                 }
                 res.send(obj)
                 return
             }
-            id.splice(0, 0, {id:reqId})
+            id.splice(0, 0, { id: reqId })
             id = JSON.stringify(id)
             console.log(id)
             base('user').update([
@@ -243,7 +243,7 @@ app.post('/addToCart', (req, res) => {
                 if (err) {
                     console.error(err);
                     return;
-                }else{
+                } else {
                     res.send(records[0].fields.cart)
                 }
             });
@@ -269,20 +269,24 @@ app.post('/deleteCart', (req, res) => {
         console.log(id)
         base('user').update([
             {
-              "id": `${req.body.user_id}`,
-              "fields": {
-                "cart": `${id}`
-              }
+                "id": `${req.body.user_id}`,
+                "fields": {
+                    "cart": `${id}`
+                }
             }
-          ], function(err, records) {
+        ], function (err, records) {
             if (err) {
-              console.error(err);
-              return;
+                console.error(err);
+                return;
             }
-            records.forEach(function(record) {
-              res.send(`Item Removed: ${reqId}`)
+            records.forEach(function (record) {
+                let obj = {
+                    msg: `Item Removed : ${reqId}`,
+                    res: true
+                }
+                res.send(obj)
             });
-          });
+        });
     })
 })
 
@@ -299,7 +303,7 @@ app.get('/cart', (req, res) => {
             }
             let id = JSON.parse(records[0].fields.cart)
             // console.log(id)
-            id.forEach(function (record,idx) {
+            id.forEach(function (record, idx) {
                 base('product').select({
                     filterByFormula: `id = "${record.id}"`
                 }).firstPage(function (err, records) {
@@ -308,11 +312,11 @@ app.get('/cart', (req, res) => {
                         return;
                     }
                     data.push(records[0].fields)
-                    if(idx === id.length-1){
+                    if (idx === id.length - 1) {
                         res.send(data)
                     }
                 })
-             })
+            })
 
         });
     } catch (err) {
@@ -324,14 +328,14 @@ app.get('/cart', (req, res) => {
 app.post('/addToWishlist', (req, res) => {
     base('user').find(`${req.body.user_id}`, function (err, record) {
         const reqId = req.body.id
-        if (err) { 
+        if (err) {
             res.status(404)
             res.send('user not found')
             console.error(err)
             return
         }
         if (!record.get('wishlist')) {
-            let obj = [{id: reqId}]
+            let obj = [{ id: reqId }]
             let id = JSON.stringify(obj)
             console.log(id)
             base('user').update([
@@ -346,22 +350,22 @@ app.post('/addToWishlist', (req, res) => {
                     console.error(err);
                     res.send(err)
                     return;
-                }else{
+                } else {
                     res.send('Added to wishlist')
                 }
 
             });
         } else {
             let id = JSON.parse(record.get('wishlist'))
-            let checkDuplicate = id.find(rec => rec.id === `${reqId}`) 
-            if(checkDuplicate){
+            let checkDuplicate = id.find(rec => rec.id === `${reqId}`)
+            if (checkDuplicate) {
                 let obj = {
                     msg: 'product already in wishlist'
                 }
                 res.send(obj)
                 return
             }
-            id.splice(0, 0, {id:reqId})
+            id.splice(0, 0, { id: reqId })
             id = JSON.stringify(id)
             console.log(id)
             base('user').update([
@@ -375,7 +379,7 @@ app.post('/addToWishlist', (req, res) => {
                 if (err) {
                     console.error(err);
                     return;
-                }else{
+                } else {
                     res.send(records[0].fields.wishlist)
                 }
             });
@@ -401,20 +405,24 @@ app.post('/deleteWishlist', (req, res) => {
         console.log(id)
         base('user').update([
             {
-              "id": `${req.body.user_id}`,
-              "fields": {
-                "wishlist": `${id}`
-              }
+                "id": `${req.body.user_id}`,
+                "fields": {
+                    "wishlist": `${id}`
+                }
             }
-          ], function(err, records) {
+        ], function (err, records) {
             if (err) {
-              console.error(err);
-              return;
+                console.error(err);
+                return;
             }
-            records.forEach(function(record) {
-              res.send(`Item Removed: ${reqId}`)
+            records.forEach(function (record) {
+                let obj = {
+                    msg: `Item Removed : ${reqId}`,
+                    res: true
+                }
+                res.send(obj)
             });
-          });
+        });
     })
 })
 
@@ -431,7 +439,7 @@ app.get('/wishlist', (req, res) => {
             }
             let id = JSON.parse(records[0].fields.wishlist)
             console.log(id)
-            id.forEach(function (record,idx) {
+            id.forEach(function (record, idx) {
                 base('product').select({
                     filterByFormula: `id = "${record.id}"`
                 }).firstPage(function (err, records) {
@@ -440,11 +448,11 @@ app.get('/wishlist', (req, res) => {
                         return;
                     }
                     data.push(records[0].fields)
-                    if(idx === id.length-1){
+                    if (idx === id.length - 1) {
                         res.send(data)
                     }
                 })
-             })
+            })
 
         });
     } catch (err) {
